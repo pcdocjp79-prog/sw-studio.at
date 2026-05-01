9d0b7a3ec8c7adee69c9c6c153f886ce73b59110
pcdocjp79-prog <pcdocjp79@gmail.com>
2026-05-01 16:59:27 +0200

    fix(hero): analytical normals for water spheres + slower motion

--- src/js/water-sphere.js
+++ src/js/water-sphere.js
@@ -60,10 +60,62 @@ varying float vDisp;
 
 ${SIMPLEX_NOISE_GLSL}
 
-float liquidDisp(vec3 p) {
-  float n1 = snoise(p * 0.55 + vec3(0.0, uTime * 0.12, 0.0));
-  float n2 = snoise(p * 1.10 + vec3(uTime * 0.08, 0.0, uTime * 0.06)) * 0.28;
-  return (n1 + n2) * 0.22;
+vec4 snoiseD(vec3 v) {
+  const vec2 C = vec2(1.0/6.0, 1.0/3.0);
+  const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
+  vec3 i  = floor(v + dot(v, C.yyy));
+  vec3 x0 = v - i + dot(i, C.xxx);
+  vec3 g = step(x0.yzx, x0.xyz);
+  vec3 l = 1.0 - g;
+  vec3 i1 = min(g.xyz, l.zxy);
+  vec3 i2 = max(g.xyz, l.zxy);
+  vec3 x1 = x0 - i1 + C.xxx;
+  vec3 x2 = x0 - i2 + C.yyy;
+  vec3 x3 = x0 - D.yyy;
+  i = mod289(i);
+  vec4 p = permute(permute(permute(
+              i.z + vec4(0.0, i1.z, i2.z, 1.0))
+            + i.y + vec4(0.0, i1.y, i2.y, 1.0))
+            + i.x + vec4(0.0, i1.x, i2.x, 1.0));
+  float n_ = 0.142857142857;
+  vec3 ns = n_ * D.wyz - D.xzx;
+  vec4 j = p - 49.0 * floor(p * ns.z * ns.z);
+  vec4 x_ = floor(j * ns.z);
+  vec4 y_ = floor(j - 7.0 * x_);
+  vec4 x = x_ * ns.x + ns.yyyy;
+  vec4 y = y_ * ns.x + ns.yyyy;
+  vec4 h = 1.0 - abs(x) - abs(y);
+  vec4 b0 = vec4(x.xy, y.xy);
+  vec4 b1 = vec4(x.zw, y.zw);
+  vec4 s0 = floor(b0) * 2.0 + 1.0;
+  vec4 s1 = floor(b1) * 2.0 + 1.0;
+  vec4 sh = -step(h, vec4(0.0));
+  vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
+  vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
+  vec3 g0 = vec3(a0.xy, h.x);
+  vec3 g1 = vec3(a0.zw, h.y);
+  vec3 g2 = vec3(a1.xy, h.z);
+  vec3 g3 = vec3(a1.zw, h.w);
+  vec4 norm = taylorInvSqrt(vec4(dot(g0,g0), dot(g1,g1), dot(g2,g2), dot(g3,g3)));
+  g0 *= norm.x; g1 *= norm.y; g2 *= norm.z; g3 *= norm.w;
+  vec4 m = max(0.6 - vec4(dot(x0,x0), dot(x1,x1), dot(x2,x2), dot(x3,x3)), 0.0);
+  vec4 m2 = m * m;
+  vec4 m3 = m2 * m;
+  vec4 m4 = m2 * m2;
+  vec4 pdotx = vec4(dot(g0, x0), dot(g1, x1), dot(g2, x2), dot(g3, x3));
+  float value = 42.0 * dot(m4, pdotx);
+  vec3 grad = -8.0 * (m3.x * pdotx.x * x0 + m3.y * pdotx.y * x1 + m3.z * pdotx.z * x2 + m3.w * pdotx.w * x3)
+            + (m4.x * g0 + m4.y * g1 + m4.z * g2 + m4.w * g3);
+  grad *= 42.0;
+  return vec4(value, grad);
+}
+
+vec4 liquidDispWithGrad(vec3 p) {
+  vec4 n1 = snoiseD(p * 0.55 + vec3(0.0, uTime * 0.12, 0.0));
+  vec4 n2 = snoiseD(p * 1.10 + vec3(uTime * 0.08, 0.0, uTime * 0.06));
+  float value = (n1.x + n2.x * 0.28) * 0.22;
+  vec3 grad = (n1.yzw * 0.55 + n2.yzw * 1.10 * 0.28) * 0.22;
+  return vec4(value, grad);
 }
 
 void main() {
@@ -72,7 +124,9 @@ void main() {
   vec4 viewPosInit = modelViewMatrix * vec4(pos, 1.0);
   vec3 viewP = viewPosInit.xyz;
 
-  float disp = liquidDisp(pos);
+  vec4 dispData = liquidDispWithGrad(pos);
+  float disp = dispData.x;
+  vec3 dispGrad = dispData.yzw;
 
   vec2 screenXY = viewP.xy;
   vec2 pokeDelta = (screenXY - uPokeTrail) / max(uOrbScale, 0.001);
@@ -94,14 +148,8 @@ void main() {
 
   vec3 displaced = pos + normal * disp;
 
-  float eps = 0.01;
-  vec3 tangent1 = normalize(cross(normal, vec3(0.0, 1.0, 0.0) + vec3(0.001)));
-  vec3 tangent2 = normalize(cross(normal, tangent1));
-  vec3 p1 = pos + tangent1 * eps;
-  vec3 p2 = pos + tangent2 * eps;
-  vec3 d1 = p1 + normal * liquidDisp(p1);
-  vec3 d2 = p2 + normal * liquidDisp(p2);
-  vec3 newNormal = normalize(cross(d1 - displaced, d2 - displaced));
+  vec3 tanGrad = dispGrad - dot(dispGrad, normal) * normal;
+  vec3 newNormal = normalize(normal - tanGrad);
 
   vec4 mv = modelViewMatrix * vec4(displaced, 1.0);
   vViewPos = -mv.xyz;
